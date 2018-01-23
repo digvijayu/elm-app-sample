39887def1a9dfa4511aa849d0460cf8fe8227ff6
Digvijay Upadhyay <digvijay.u20@gmail.com>
2018-01-23 12:25:37 +0000

    Created holiday apps

---
+++ HolidaysApp/src/App.elm
@@ -0,0 +1,54 @@
+module App exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+main : Html msg
+main =
+  view
+
+view : Html msg
+view =
+  div [ class "container col-md-4 order-md-2 mb-4" ]
+    [ h4 [ class "d-flex justify-content-between align-items-center mb-3" ]
+        [ span [ class "text-muted" ]
+            [ text "3 Holidays Found" ]
+        ]
+    , ul [ class "list-group mb-3" ]
+        [ li [ class "list-group-item d-flex justify-content-between lh-condensed" ]
+            [ div []
+                [ h6 [ class "my-0" ]
+                    [ text "Sandos Papagayo Beach Resort" ]
+                , small [ class "text-muted" ]
+                    [ text "Spain, Canary Islands, Lanzarote, Playa Blanca" ]
+                ]
+            , span [ class "text-muted" ]
+                [ text "£864 pp" ]
+            ]
+        , li [ class "list-group-item d-flex justify-content-between lh-condensed" ]
+            [ div []
+                [ h6 [ class "my-0" ]
+                    [ text "Barcelo Teguise Beach" ]
+                , small [ class "text-muted" ]
+                    [ text "Spain, Canary Islands, Lanzarote, Costa Teguise" ]
+                ]
+            , span [ class "text-muted" ]
+                [ text ("£1,307 pp") ]
+            ]
+        , li [ class "list-group-item d-flex justify-content-between lh-condensed" ]
+            [ div []
+                [ h6 [ class "my-0" ]
+                    [ text "SENTIDO Aequora Lanzarote" ]
+                , small [ class "text-muted" ]
+                    [ text "Spain, Canary Islands, Lanzarote, Los Pocillos" ]
+                ]
+            , span [ class "text-muted" ]
+                [ text "£768 pp" ]
+            ]
+        ]
+    , Html.form [ class "card p-2" ]
+        [ div [ class "input-group" ]
+            [ button [ class "btn btn-lg btn-primary", attribute "role" "button" ]
+                [ text "Currency Exchange" ]
+            ]
+        ]
+    ]
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # elm-app-sample
 Sample architecture to re-use elm components
+
+elm-make ForexApp/src/App.elm --output=ForexApp/app.js
+
+elm-make HolidaysApp/src/App.elm --output=HolidaysApp/app.js
